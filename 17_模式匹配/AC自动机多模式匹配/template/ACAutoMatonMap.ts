5b8dc822e5915d1dcf1e019ca10361aa44830d18
草莓奶昔 <lmt2818088@gmail.com>
2023-11-18 23:25:23 +0800

    🐞 fix(ACAutoMatonMap):

--- 17_模式匹配/AC自动机多模式匹配/template/ACAutoMatonMap.ts
+++ 17_模式匹配/AC自动机多模式匹配/template/ACAutoMatonMap.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-inner-declarations */
 /* eslint-disable no-loop-func */
 /* eslint-disable max-len */
 
@@ -80,66 +81,117 @@ class ACAutoMatonMap {
 
   getCounter(): Uint32Array {
     const counter = new Uint32Array(this._children.length)
-    for (let i=
-    for (let v of this._bfsOrder) {
+    this.wordPos.forEach(pos => {
+      counter[pos]++
+    })
+    this._bfsOrder.forEach(v => {
       if (v !== 0) {
         counter[v] += counter[this._suffixLink[v]]
       }
-    }
+    })
     return counter
   }
 
   getIndexes(): number[][] {
-    let res = new Array(this._children.length).fill([])
+    let res: number[][] = Array(this._children.length)
+    for (let i = 0; i < res.length; i++) res[i] = []
     for (let i = 0; i < this.wordPos.length; i++) {
       let pos = this.wordPos[i]
       res[pos].push(i)
     }
-    for (let v of this._bfsOrder) {
+    this._bfsOrder.forEach(v => {
       if (v !== 0) {
-        let from = this._suffixLink[v],
-          to = v
-        let arr1 = res[from],
-          arr2 = res[to]
-        let arr3 = []
-        let i = 0,
-          j = 0
+        const from = this._suffixLink[v]
+        const arr1 = res[from]
+        const arr2 = res[v]
+        const arr3 = Array(arr1.length + arr2.length)
+        let i = 0
+        let j = 0
+        let k = 0
         while (i < arr1.length && j < arr2.length) {
           if (arr1[i] < arr2[j]) {
-            arr3.push(arr1[i])
-            i++
+            arr3[k++] = arr1[i++]
           } else if (arr1[i] > arr2[j]) {
-            arr3.push(arr2[j])
-            j++
+            arr3[k++] = arr2[j++]
           } else {
-            arr3.push(arr1[i])
+            arr3[k++] = arr1[i]
             i++
             j++
           }
         }
-        while (i < arr1.length) {
-          arr3.push(arr1[i])
-          i++
-        }
-        while (j < arr2.length) {
-          arr3.push(arr2[j])
-          j++
-        }
-        res[to] = arr3
+        while (i < arr1.length) arr3[k++] = arr1[i++]
+        while (j < arr2.length) arr3[k++] = arr2[j++]
+        res[v] = arr3
       }
+    })
+    return res
+  }
+
+  dp(f: (from: number, to: number) => void): void {
+    this._bfsOrder.forEach(v => {
+      if (v !== 0) {
+        f(this._suffixLink[v], v)
+      }
+    })
+  }
+
+  get size(): number {
+    return this._children.length
+  }
+}
+
+export { ACAutoMatonMap }
+
+if (require.main === module) {
+  // https://leetcode.cn/problems/length-of-the-longest-valid-substring/description/
+  function longestValidSubstring(word: string, forbidden: string[]): number {
+    const acm = new ACAutoMatonMap()
+    forbidden.forEach(w => acm.addString(w))
+    acm.buildSuffixLink()
+
+    const minLen = new Uint32Array(acm.size).fill(-1)
+    for (let i = 0; i < forbidden.length; i++) {
+      minLen[acm.wordPos[i]] = Math.min(minLen[acm.wordPos[i]], forbidden[i].length)
+    }
+    acm.dp((from, to) => {
+      minLen[to] = Math.min(minLen[to], minLen[from])
+    })
+
+    let res = 0
+    let left = 0
+    let pos = 0
+    for (let right = 0; right < word.length; right++) {
+      pos = acm.move(pos, word.charCodeAt(right))
+      left = Math.max(left, right - minLen[pos] + 2)
+      res = Math.max(res, right - left + 1)
     }
     return res
   }
 
-  dp(f: (from: number, to: number) => void) {
-    for (let v of this._bfsOrder) {
-      if (v !== 0) {
-        f(this._suffixLink[v], v)
-      }
+  // 1032. 字符流
+  // https://leetcode.cn/problems/stream-of-characters/description/
+  class StreamChecker {
+    private readonly _acm: ACAutoMatonMap
+    private readonly _counter: Uint32Array
+    private _pos = 0
+
+    constructor(words: string[]) {
+      const acm = new ACAutoMatonMap()
+      words.forEach(w => acm.addString(w))
+      acm.buildSuffixLink()
+      this._acm = acm
+      this._counter = acm.getCounter()
+    }
+
+    query(letter: string): boolean {
+      this._pos = this._acm.move(this._pos, letter.charCodeAt(0))
+      return this._counter[this._pos] > 0
     }
   }
 
-  size(): number {
-    return this._children.length
-  }
+  /**
+   * Your StreamChecker object will be instantiated and called as such:
+   * var obj = new StreamChecker(words)
+   * var param_1 = obj.query(letter)
+   */
 }
