1bbe0658d24761bca73a8a3b8a558831d24105c9
草莓奶昔 <lmt2818088@gmail.com>
2024-03-13 02:23:18 +0800

    ✨ feat(add note):

--- 17_模式匹配/后缀平衡树/main.go
+++ 17_模式匹配/后缀平衡树/main.go
@@ -2,11 +2,13 @@
 // https://oi-wiki.org/ds/sgt/
 // https://github.com/EndlessCheng/codeforces-go/blob/49f6570d86c17f5064a5b079360f4128acc520c4/copypasta/scapegoat_tree.go#L4
 //
-// 后缀平衡树
+// 后缀平衡树(动态后缀数组)
 // https://oi-wiki.org/string/suffix-bst/
 // https://www.cnblogs.com/cjfdf/p/10322533.html
+// https://www.luogu.com.cn/article/wq2yn9lr
 // https://www.luogu.com/article/1l23lxcd
 // https://blog.csdn.net/YxuanwKeith/article/details/52741250
+// 后缀平衡树支持在字符串开头删除一个字符、复杂度不依赖于字符集的大小.
 
 package main
 
@@ -15,11 +17,14 @@ import (
 	"os"
 )
 
-// 后缀平衡树(动态后缀数组)
+// P5353 树上后缀排序
 // https://www.luogu.com.cn/problem/P5353
 func main() {
 
 }
+
+// P6164 【模板】后缀平衡树
+// https://www.luogu.com.cn/problem/P6164
 
 // P5346 【XR-1】柯南家族
 // https://www.luogu.com.cn/problem/P5346
