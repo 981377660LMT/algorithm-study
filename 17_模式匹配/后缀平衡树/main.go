a4fe520d47a3291d9b62f1306cf190a235731527
草莓奶昔 <lmt2818088@gmail.com>
2024-03-22 00:00:54 +0800

    ✨ feat(add todo):

--- 17_模式匹配/后缀平衡树/main.go
+++ 17_模式匹配/后缀平衡树/main.go
@@ -3,8 +3,10 @@
 // https://github.com/EndlessCheng/codeforces-go/blob/49f6570d86c17f5064a5b079360f4128acc520c4/copypasta/scapegoat_tree.go#L4
 //
 // 后缀平衡树(动态后缀数组)
+// 陈立杰 <<重量平衡树和后缀平衡树在信息学奥赛中的应用>> 2013论文集
 // https://oi-wiki.org/string/suffix-bst/
 // https://www.cnblogs.com/cjfdf/p/10322533.html
+// https://www.luogu.com.cn/article/0e8jjpqk
 // https://www.luogu.com.cn/article/wq2yn9lr
 // https://www.luogu.com/article/1l23lxcd
 // https://blog.csdn.net/YxuanwKeith/article/details/52741250
