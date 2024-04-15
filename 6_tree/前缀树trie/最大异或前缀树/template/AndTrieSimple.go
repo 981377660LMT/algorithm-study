76eec91f36acde2bcb3ff9e3e75f5aa9cfd370d4
草莓奶昔 <lmt2818088@gmail.com>
2024-04-16 01:58:47 +0800

    ✨ feat(AndTrieSimple):

---
+++ 6_tree/前缀树trie/最大异或前缀树/template/AndTrieSimple.go
@@ -0,0 +1,118 @@
+// 按位与最大的二元组
+// !给定一个数组，要求找到两个不同的下标i!=j使得A[i]&A[j]最大.
+
+package main
+
+import (
+	"fmt"
+	"math/bits"
+	"math/rand"
+)
+
+func main() {
+	solve1 := func(nums []int) int {
+		trie := NewAndTrie(maxs(nums))
+		res := 0
+		for _, cur := range nums {
+			res = max(res, trie.Query(cur))
+			trie.Insert(cur)
+		}
+		return res
+	}
+
+	solve2 := func(nums []int) int {
+		res := 0
+		for i := 0; i < len(nums); i++ {
+			for j := i + 1; j < len(nums); j++ {
+				res = max(res, nums[i]&nums[j])
+			}
+		}
+		return res
+	}
+
+	for i := 0; i < 1000; i++ {
+		nums := make([]int, 5)
+		for j := range nums {
+			nums[j] = rand.Intn(10)
+		}
+		res1, res2 := solve1(nums), solve2(nums)
+		if res1 != res2 {
+			fmt.Println(res1, res2, nums)
+			panic("not equal")
+		}
+	}
+}
+
+type Node struct {
+	count    int32
+	children [2]*Node
+}
+
+type AndTrieSimple struct {
+	bit  int32
+	root *Node
+}
+
+func NewAndTrie(upper int) *AndTrieSimple {
+	return &AndTrieSimple{
+		bit:  int32(bits.Len(uint(upper))),
+		root: &Node{},
+	}
+}
+
+func (bt *AndTrieSimple) Insert(num int) {
+	root := bt.root
+	for i := bt.bit - 1; i >= 0; i-- {
+		bit := (num >> i) & 1
+		if root.children[bit] == nil {
+			root.children[bit] = &Node{}
+		}
+		root = root.children[bit]
+		root.count++
+	}
+	return
+}
+
+// 必须保证num存在于trie中.
+func (bt *AndTrieSimple) Remove(num int) {
+	root := bt.root
+	for i := bt.bit - 1; i >= 0; i-- {
+		bit := (num >> i) & 1
+		root = root.children[bit]
+		root.count--
+	}
+}
+
+func (bt *AndTrieSimple) Query(num int) (maxAnd int) {
+	root := bt.root
+	for i := bt.bit - 1; i >= 0; i-- {
+		if root == nil {
+			return
+		}
+		bit := (num >> i) & 1
+		if bit == 1 && root.children[bit] != nil && root.children[bit].count > 0 {
+			maxAnd |= 1 << i
+		}
+		root = root.children[bit]
+	}
+	return
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxs(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	res := nums[0]
+	for _, num := range nums {
+		if num > res {
+			res = num
+		}
+	}
+	return res
+}
