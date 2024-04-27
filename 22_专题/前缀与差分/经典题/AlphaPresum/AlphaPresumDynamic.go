cff5d1526cfbf37dad19d50ff60b7444f4818c78
liaomingtian <liaomingtian@meituan.com>
2024-04-28 03:27:05 +0800

    ✨ feat(AlphaPresumDynamic):

--- 22_专题/前缀与差分/经典题/AlphaPresum/AlphaPresumDynamic.go
+++ 22_专题/前缀与差分/经典题/AlphaPresum/AlphaPresumDynamic.go
@@ -12,7 +12,9 @@ package main
 
 import (
 	"fmt"
+	"math/bits"
 	"math/rand"
+	"strings"
 )
 
 func main() {
@@ -25,7 +27,10 @@ const OFFSET int32 = 97
 type AlphaPresumDynamic struct {
 	n    int32
 	arr  []int32
-	tree [SIGMA]*BITArray32
+	tree [SIGMA]*bITArray01
+
+	revTree      [SIGMA]*bITArray01
+	revTreeBuilt bool
 }
 
 func NewAlphaPresumDynamic(n int32, f func(i int32) int32) *AlphaPresumDynamic {
@@ -33,17 +38,19 @@ func NewAlphaPresumDynamic(n int32, f func(i int32) int32) *AlphaPresumDynamic {
 	for i := int32(0); i < n; i++ {
 		arr[i] = f(i) - OFFSET
 	}
-	tree := [SIGMA]*BITArray32{}
+	tree := [SIGMA]*bITArray01{}
 	for i := int32(0); i < SIGMA; i++ {
-		tree[i] = NewBitArray(n)
+		tree[i] = newBITArray01(n)
 	}
 	for i := int32(0); i < n; i++ {
-		tree[arr[i]].Add(i, 1)
+		tree[arr[i]].Add(i)
 	}
 	res := &AlphaPresumDynamic{n: n, arr: arr, tree: tree}
 	return res
 }
 
+// 如果一个范围内的元素是单调递增的，那么对于这个范围内的任何字符，
+// 它们出现的位置应该是连续的，且随着字符值的增加，出现的位置也应该是递增的.
 func (a *AlphaPresumDynamic) IsAscending(start, end int32) bool {
 	if start < 0 {
 		start = 0
@@ -54,6 +61,7 @@ func (a *AlphaPresumDynamic) IsAscending(start, end int32) bool {
 	if start >= end {
 		return true
 	}
+
 	ptr := start
 	for i := int32(0); i < SIGMA; i++ {
 		c := a.tree[i].QueryRange(start, end)
@@ -75,11 +83,13 @@ func (a *AlphaPresumDynamic) IsDescending(start, end int32) bool {
 	if start >= end {
 		return true
 	}
-	ptr := end - 1
-	for i := SIGMA - 1; i >= 0; i-- {
-		c := a.tree[i].QueryRange(start, end)
-		ptr -= c
-		if ptr < start || a.tree[i].QueryRange(ptr, end) != c {
+
+	a.buildRevTree()
+	ptr := start
+	for i := int32(0); i < SIGMA; i++ {
+		c := a.revTree[i].QueryRange(start, end)
+		ptr += c
+		if ptr > end || a.revTree[i].QueryRange(start, ptr) != c {
 			return false
 		}
 	}
@@ -128,9 +138,13 @@ func (a *AlphaPresumDynamic) Set(index, value int32) {
 		return
 	}
 	value -= OFFSET
-	a.tree[oldValue].Add(index, -1)
-	a.tree[value].Add(index, 1)
+	a.tree[oldValue].Remove(index)
+	a.tree[value].Add(index)
 	a.arr[index] = value
+	if a.revTreeBuilt {
+		a.revTree[SIGMA-1-oldValue].Remove(index)
+		a.revTree[SIGMA-1-value].Add(index)
+	}
 }
 
 func (a *AlphaPresumDynamic) Get(index int32) int32 {
@@ -167,19 +181,32 @@ func (a *AlphaPresumDynamic) CountAll(start, end int32) [SIGMA]int32 {
 	return res
 }
 
+func (a *AlphaPresumDynamic) buildRevTree() {
+	if a.revTreeBuilt {
+		return
+	}
+	for i := int32(0); i < SIGMA; i++ {
+		a.revTree[i] = newBITArray01(a.n)
+	}
+	for i := int32(0); i < a.n; i++ {
+		a.revTree[SIGMA-1-a.arr[i]].Add(i)
+	}
+	a.revTreeBuilt = true
+}
+
 // !Point Add Range Sum, 0-based.
-type BITArray32 struct {
+type bITArray32 struct {
 	n     int32
 	total int32
 	data  []int32
 }
 
-func NewBitArray(n int32) *BITArray32 {
-	res := &BITArray32{n: n, data: make([]int32, n)}
+func newBitArray(n int32) *bITArray32 {
+	res := &bITArray32{n: n, data: make([]int32, n)}
 	return res
 }
 
-func NewBitArrayFrom(n int32, f func(i int32) int32) *BITArray32 {
+func newBitArrayFrom(n int32, f func(i int32) int32) *bITArray32 {
 	total := int32(0)
 	data := make([]int32, n)
 	for i := int32(0); i < n; i++ {
@@ -192,10 +219,10 @@ func NewBitArrayFrom(n int32, f func(i int32) int32) *BITArray32 {
 			data[j-1] += data[i-1]
 		}
 	}
-	return &BITArray32{n: n, total: total, data: data}
+	return &bITArray32{n: n, total: total, data: data}
 }
 
-func (b *BITArray32) Add(index int32, v int32) {
+func (b *bITArray32) Add(index int32, v int32) {
 	b.total += v
 	for index++; index <= b.n; index += index & -index {
 		b.data[index-1] += v
@@ -203,7 +230,7 @@ func (b *BITArray32) Add(index int32, v int32) {
 }
 
 // [0, end).
-func (b *BITArray32) QueryPrefix(end int32) int32 {
+func (b *bITArray32) QueryPrefix(end int32) int32 {
 	if end > b.n {
 		end = b.n
 	}
@@ -215,7 +242,7 @@ func (b *BITArray32) QueryPrefix(end int32) int32 {
 }
 
 // [start, end).
-func (b *BITArray32) QueryRange(start, end int32) int32 {
+func (b *bITArray32) QueryRange(start, end int32) int32 {
 	if start < 0 {
 		start = 0
 	}
@@ -240,8 +267,98 @@ func (b *BITArray32) QueryRange(start, end int32) int32 {
 	return pos - neg
 }
 
-func (b *BITArray32) QueryAll() int32 {
+func (b *bITArray32) QueryAll() int32 {
 	return b.total
+}
+
+// 01树状数组.
+type bITArray01 struct {
+	n    int32
+	size int32 // data、bit的长度
+	data []uint64
+	bit  *bITArray32
+}
+
+func newBITArray01(n int32) *bITArray01 {
+	return newBITArray01From(n, func(index int32) bool { return false })
+}
+
+func newBITArray01From(n int32, f func(index int32) bool) *bITArray01 {
+	size := n>>6 + 1
+	data := make([]uint64, size)
+	for i := int32(0); i < n; i++ {
+		if f(i) {
+			data[i>>6] |= 1 << (i & 63)
+		}
+	}
+	bit := newBitArrayFrom(size, func(i int32) int32 { return int32(bits.OnesCount64(data[i])) })
+	return &bITArray01{n: n, size: size, data: data, bit: bit}
+}
+
+func (bit01 *bITArray01) QueryAll() int32 {
+	return bit01.bit.QueryAll()
+}
+
+func (bit01 *bITArray01) QueryPrefix(end int32) int32 {
+	i, j := end>>6, end&63
+	res := bit01.bit.QueryPrefix(i)
+	res += int32(bits.OnesCount64(bit01.data[i] & ((1 << j) - 1)))
+	return res
+}
+
+func (bit01 *bITArray01) QueryRange(start, end int32) int32 {
+	if start < 0 {
+		start = 0
+	}
+	if end > bit01.n {
+		end = bit01.n
+	}
+	if start >= end {
+		return 0
+	}
+	if start == 0 {
+		return bit01.QueryPrefix(end)
+	}
+	res := int32(0)
+	res -= int32(bits.OnesCount64(bit01.data[start>>6] & ((1 << (start & 63)) - 1)))
+	res += int32(bits.OnesCount64(bit01.data[end>>6] & ((1 << (end & 63)) - 1)))
+	res += bit01.bit.QueryRange(start>>6, end>>6)
+	return res
+}
+
+func (bit01 *bITArray01) Add(index int32) bool {
+	i, j := index>>6, index&63
+	if (bit01.data[i]>>j)&1 == 1 {
+		return false
+	}
+	bit01.data[i] |= 1 << j
+	bit01.bit.Add(i, 1)
+	return true
+}
+
+func (bit01 *bITArray01) Remove(index int32) bool {
+	i, j := index>>6, index&63
+	if (bit01.data[i]>>j)&1 == 0 {
+		return false
+	}
+	bit01.data[i] ^= 1 << j
+	bit01.bit.Add(i, -1)
+	return true
+}
+
+func (bit01 *bITArray01) Has(index int32) bool {
+	i, j := index>>6, index&63
+	return (bit01.data[i]>>j)&1 == 1
+}
+
+func (bit01 *bITArray01) String() string {
+	res := []string{}
+	for i := int32(0); i < bit01.n; i++ {
+		if bit01.QueryRange(i, i+1) == 1 {
+			res = append(res, fmt.Sprintf("%d", i))
+		}
+	}
+	return fmt.Sprintf("BITArray01: [%v]", strings.Join(res, ", "))
 }
 
 func test() {
@@ -338,9 +455,10 @@ func test() {
 			if S.IsAscending(start, end) != isAscendingBruteForce(start, end) {
 				panic("IsAscending Error")
 			}
-			// if S.IsDescending(start, end) != isDescendingBruteForce(start, end) {
-			// 	panic("IsDescending Error")
-			// }
+			if S.IsDescending(start, end) != isDescendingBruteForce(start, end) {
+				fmt.Println(start, end, S.IsDescending(start, end), isDescendingBruteForce(start, end))
+				panic("IsDescending Error")
+			}
 
 			if S.Min(start, end) != minBruteForce(start, end) {
 				fmt.Println(start, end)
