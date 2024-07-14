c841b5fb6589162049cc7349098d2245e591b3a9
草莓奶昔 <lmt2818088@gmail.com>
2024-07-15 01:52:06 +0800

    ✨ feat(abc362):

--- 11_动态规划/子数组/等差数列/note.md
+++ 11_动态规划/子数组/等差数列/note.md
@@ -1,7 +1,7 @@
 `446. 等差数列划分 II - 子序列 copy.py `
 `1027. 最长等差数列.py`
 
-dp[i][diff] 表示以 nums[i] 结尾，公差为 diff 的等差数列的个数。
+`等差数列dp`：dp[i][diff] 表示以 nums[i] 结尾，公差为 diff 的等差数列的个数。
 
 ```Python
 dp = [defaultdict(int) for _ in range(n)]
--- 6_tree/树的性质/树的重心.go
+++ 6_tree/树的性质/树的重心.go
@@ -12,11 +12,12 @@ func main() {
 		{2},
 	}
 	root := 0
-	centroids := findCentroids(n, tree, root)
+	centroids := FindCentroids(n, tree, root)
 	fmt.Println(centroids)
 }
 
-func findCentroids(n int, tree [][]int, root int) (centroids []int) {
+// getCentroids
+func FindCentroids(n int, tree [][]int, root int) (centroids []int) {
 	weight := make([]int, n)
 	subSize := make([]int, n)
 	var dfs func(cur, pre int)
@@ -41,6 +42,37 @@ func findCentroids(n int, tree [][]int, root int) (centroids []int) {
 }
 
 func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func FindCentroids32(n int32, tree [][]int32, root int32) (centroids []int32) {
+	weight := make([]int32, n)
+	subSize := make([]int32, n)
+	var dfs func(cur, pre int32)
+	dfs = func(cur, pre int32) {
+		subSize[cur] = 1
+		for _, next := range tree[cur] {
+			if next == pre {
+				continue
+			}
+			dfs(next, cur)
+			subSize[cur] += subSize[next]
+			weight[cur] = max32(weight[cur], subSize[next])
+		}
+		weight[cur] = max32(weight[cur], n-subSize[cur])
+		if weight[cur] <= n/2 {
+			centroids = append(centroids, cur)
+		}
+	}
+
+	dfs(root, -1)
+	return
+}
+
+func max32(a, b int32) int32 {
 	if a > b {
 		return a
 	}
