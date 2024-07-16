66014db944515ae337b7028a369cb30e818e2d26
liaomingtian <liaomingtian@meituan.com>
2024-07-16 14:06:52 +0800

    ✨ feat(add note):

---
+++ 7_graph/网络流/3-最小割应用/动态全局最小割.go
@@ -0,0 +1,12 @@
+// 动态全局最小割(最小割树)
+// https://ei1333.github.io/library/graph/flow/global-minimum-cut-of-dynamic-star-augmented-graph.hpp
+// 一个n个点的无向图上，两点之间本质不同的最小割只有n-1种，因此一定存在一棵树，满足树上两点的最小割等于原图上两点的最小割
+
+package main
+
+// https://judge.yosupo.jp/problem/global_minimum_cut_of_dynamic_star_augmented_graph
+// https://www.luogu.com.cn/problem/P4897
+// https://www.luogu.com.cn/problem/P5632
+func main() {
+
+}
