ace17e661faec060d7be562b25ec47ffb910ae79
草莓奶昔 <lmt2818088@gmail.com>
2024-12-19 23:47:07 +0800

    ✨ feat: 更新章节29的子类方法表重建策略，增加中文注释，优化元编程讨论

---
+++ chore/js/crafting interpreters/answers/chapter29_superclasses/延迟更新.ts
@@ -0,0 +1,42 @@
+interface IDelegator {
+  mutate(): void
+  query(): void
+  pushDown(): void
+}
+
+/**
+ * 使用版本机制延迟更新子树.
+ */
+class Node {
+  private readonly _parent: Node | undefined
+  private readonly _delegator: IDelegator
+  private _version = 0
+  private _parentVersion = 0
+
+  constructor(parent: Node | undefined, delegator: IDelegator) {
+    this._parent = parent
+    if (parent) this._parentVersion = parent._version
+    this._delegator = delegator
+  }
+
+  mutate() {
+    this._version++
+    this._delegator.mutate()
+  }
+
+  query(): void {
+    this._sync()
+    this._delegator.query()
+  }
+
+  private _sync(): void {
+    if (!this._parent) return
+    this._parent._sync()
+    if (this._parentVersion !== this._parent._version) {
+      this._parentVersion = this._parent._version
+      this._parent._delegator.pushDown()
+    }
+  }
+}
+
+export {}
