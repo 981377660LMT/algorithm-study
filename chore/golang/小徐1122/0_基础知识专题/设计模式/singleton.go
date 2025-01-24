da6785cf1eafee0daeb32ab4944b1b9bb1a9b7c2
草莓奶昔 <lmt2818088@gmail.com>
2025-01-24 13:08:08 +0800

    ✨ feat: 实现单例模式，添加相关代码和文档，包含懒汉式和饿汉式示例

--- chore/golang/小徐1122/0_基础知识专题/设计模式/singleton.go
+++ chore/golang/小徐1122/0_基础知识专题/设计模式/singleton.go
@@ -5,6 +5,74 @@
 
 package main
 
+import "sync"
+
 func main() {
 
 }
+
+// #region 懒汉
+
+var (
+	s   *singleton
+	mux sync.Mutex
+)
+
+type singleton struct {
+}
+
+func newSingleton() *singleton {
+	return &singleton{}
+}
+
+func (s *singleton) Work() {
+}
+
+type Instance interface {
+	Work()
+}
+
+func GetInstance() Instance {
+	if s != nil {
+		return s
+	}
+	mux.Lock()
+	defer mux.Unlock()
+	if s != nil {
+		return s
+	}
+	s = newSingleton()
+	return s
+}
+
+// #endregion
+
+// #region sync.Once
+
+var (
+	s2   *singleton2
+	once sync.Once
+)
+
+type singleton2 struct {
+}
+
+func newSingleton2() *singleton2 {
+	return &singleton2{}
+}
+
+func (s *singleton2) Work() {
+}
+
+type Instance2 interface {
+	Work()
+}
+
+func GetInstance2() Instance2 {
+	once.Do(func() {
+		s2 = newSingleton2()
+	})
+	return s2
+}
+
+// #endregion
