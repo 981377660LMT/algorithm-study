b839ab7b663fff8d343b8c9ccbe5c6d5fddd6514
草莓奶昔 <lmt2818088@gmail.com>
2025-02-07 18:36:50 +0800

    ✨ feat(abc): 优化 K-th Largest Triplet 算法，调整排序顺序并修正条件逻辑 ✨ chore(golang): 更新 Golang 网络 IO 模型文档，添加多路复用解析及相关图片

--- 8_heap/分类/多路归并/abc-F - K-th Largest Triplet-二分.py
+++ 8_heap/分类/多路归并/abc-F - K-th Largest Triplet-二分.py
@@ -11,38 +11,40 @@ input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 
 def kthLargestTriplet(A: list[int], B: list[int], C: list[int], K: int) -> int:
-    A, B, C = sorted(A), sorted(B), sorted(C)  # type: ignore
+    A, B, C = sorted(A, reverse=True), sorted(B, reverse=True), sorted(C, reverse=True)  # type: ignore
     N = len(A)
 
     def f(i: int, j: int, k: int) -> int:
         return A[i] * B[j] + B[j] * C[k] + C[k] * A[i]
 
     def check(mid: int) -> bool:
-        """不超过mid的数<k个."""
+        """
+        不小于mid的数是否<k个.
+        时间复杂度O(K).
+        """
         count = 0
         for i in range(N):
-            if f(i, 0, 0) > mid:
+            if f(i, 0, 0) < mid:
                 break
             for j in range(N):
-                if f(i, j, 0) > mid:
+                if f(i, j, 0) < mid:
                     break
                 for k in range(N):
-                    if f(i, j, k) <= mid:
-                        count += 1
-                        if count >= K:
-                            return False
-                    else:
+                    if f(i, j, k) < mid:
                         break
+                    count += 1
+                    if count >= K:
+                        return False
         return count < K
 
-    left, right = f(0, 0, 0), f(N - 1, N - 1, N - 1)
+    left, right = f(N - 1, N - 1, N - 1), f(0, 0, 0)
     while left <= right:
         mid = (left + right) // 2
         if check(mid):
-            left = mid + 1
-        else:
             right = mid - 1
-    return left
+        else:
+            left = mid + 1
+    return right
 
 
 if __name__ == "__main__":
--- chore/golang/小徐1122/0_基础知识专题/go io 多路复用之 epoll/note.md
+++ chore/golang/小徐1122/0_基础知识专题/go io 多路复用之 epoll/note.md
@@ -1,1 +1,12 @@
 # 解析 Golang 网络 IO 模型之 EPOLL
+
+![alt text](image.png)
+
+## 1. IO 多路复用
+
+1. 何为 IO 多路复用
+   多路复用指的是，由一个执行单元，同时对多个对象提供服务，形成一对多的服务关系
+   ![alt text](image-1.png)
+   • 多路：存在多个需要处理 io event 的 fd（linux 中，一切皆文件，所有事物均可抽象为一个文件句柄 file descriptor，简称 fd）
+   • 复用：复用一个 loop thread ，同时为多个 fd 提供处理服务（线程 thread 是内核视角下的最小调度单位；多路复用通常为循环模型 loop model，因此称为 loop thread）
+2. IO 多路复用的简单实现
