e14b1ddd8121760becb344d788aaec37627eb16b
liaomingtian <liaomingtian@meituan.com>
2025-02-08 11:22:15 +0800

    ✨ feat(multileveltree): 添加多级树的查询和更新方法，初步实现功能

--- 22_专题/离线查询/根号分治/multileveltree/tree.go
+++ 22_专题/离线查询/根号分治/multileveltree/tree.go
@@ -10,8 +10,25 @@ type MultiLevelTree[E any] struct {
 	op func(a, b E) E
 }
 
-func NewMultiLevelTree[E any](e func() E, op func(a, b E) E) *MultiLevelTree[E] {
+func NewMultiLevelTree[E any](
+	e func() E, op func(a, b E) E) *MultiLevelTree[E] {
 	return &MultiLevelTree[E]{e: e, op: op}
 }
-
 func (m *MultiLevelTree[E]) Build(n int, f func(i int) E) {}
+
+func (m *MultiLevelTree[E]) QueryRange(l, r int) E {
+	panic("todo")
+}
+func (m *MultiLevelTree[E]) QueryAll() E {
+	panic("todo")
+}
+func (m *MultiLevelTree[E]) Get(i int) E {
+	panic("todo")
+}
+
+func (m *MultiLevelTree[E]) Update(i int, v E) {
+	panic("todo")
+}
+func (m *MultiLevelTree[E]) Set(i int, v E) {
+	panic("todo")
+}
