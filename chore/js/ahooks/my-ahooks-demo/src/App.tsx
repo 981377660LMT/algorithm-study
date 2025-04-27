edbcd9357ced1b0e7acf66d3c96138bac715f42a
liaomingtian <liaomingtian@meituan.com>
2025-04-27 11:29:47 +0800

    feat: 添加 mockjs 依赖并更新 useRequest 示例，增强文档内容

--- chore/js/ahooks/my-ahooks-demo/src/App.tsx
+++ chore/js/ahooks/my-ahooks-demo/src/App.tsx
@@ -1,11 +1,24 @@
 import { useRequest } from 'ahooks'
+import Mock from 'mockjs'
+
+function getUsername(): Promise<string> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(Mock.mock('@name'))
+    }, 1000)
+  })
+}
 
 function App() {
-  const { data, error, loading } = useRequest(() => fetch('https://api.github.com').then(res => res.json()))
+  const { data, error, loading, mutate } = useRequest(getUsername)
 
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error!</div>
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  if (error) {
+    return <div>failed to load</div>
+  }
+  if (loading) {
+    return <div>loading...</div>
+  }
+  return <div>Username: {data}</div>
 }
 
 export default App
