cd8a8972039090d999590e325cc36b8a40b4aa3e
草莓奶昔 <lmt2818088@gmail.com>
2025-06-08 12:42:51 +0800

    feat: 新增 AtCoder 题目 F - Athletic 的 Go 语言解法及相关文档更新

---
+++ atcoder/abc408-F - Athletic-线段树dp.go
@@ -0,0 +1,245 @@
+// F - Athletic(跳台)
+// https://atcoder.jp/contests/abc408/tasks/abc408_f
+//
+// 有编号为 1 到 N 的 N 个跳台排成一列，第 i 个跳台的高度为 H_i。
+// H_i 是1到N的排列.
+//
+// 高桥君要玩一个在跳台上移动的游戏。最开始，他可以自由选择一个跳台 i（1≤i≤N）站上去。
+//
+// 当他站在跳台 i 上时，可以选择满足以下条件的跳台 j（1≤j≤N）并跳过去：
+//
+// H_j ≤ H_i − D
+// 1 ≤ |i−j| ≤ R
+// 高桥君会不断移动，直到无法再移动为止。请问，他最多能移动多少次？
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var N, D, R int
+	fmt.Fscan(in, &N, &D, &R)
+	H := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Fscan(in, &H[i])
+		H[i]--
+	}
+
+	heightToPos := make([]int, N)
+	for i, h := range H {
+		heightToPos[h] = i
+	}
+
+	seg := NewSegmentTree(N, func(i int) int { return 0 })
+	dp := make([]int, N) // !从位置 i 开始，最多能移动多少次
+	res := 0
+	for h, pos := range heightToPos { // !按照高度从小到大处理
+		if h2 := h - D; h2 >= 0 {
+			pos2 := heightToPos[h2]
+			seg.Update(pos2, dp[pos2])
+		}
+		curMax := seg.Query(max(0, pos-R), min(N, pos+R+1))
+		dp[pos] = curMax + 1
+		res = max(res, dp[pos])
+	}
+	fmt.Fprintln(out, res-1)
+}
+
+const INF int = 1e18
+
+// PointSetRangeMax
+
+type E = int
+
+func (*SegmentTree) e() E        { return 0 }
+func (*SegmentTree) op(a, b E) E { return max(a, b) }
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+type SegmentTree struct {
+	n, size int
+	seg     []E
+}
+
+func NewSegmentTree(n int, f func(int) E) *SegmentTree {
+	res := &SegmentTree{}
+	size := 1
+	for size < n {
+		size <<= 1
+	}
+	seg := make([]E, size<<1)
+	for i := range seg {
+		seg[i] = res.e()
+	}
+	for i := 0; i < n; i++ {
+		seg[i+size] = f(i)
+	}
+	for i := size - 1; i > 0; i-- {
+		seg[i] = res.op(seg[i<<1], seg[i<<1|1])
+	}
+	res.n = n
+	res.size = size
+	res.seg = seg
+	return res
+}
+func NewSegmentTreeFrom(leaves []E) *SegmentTree {
+	res := &SegmentTree{}
+	n := len(leaves)
+	size := 1
+	for size < n {
+		size <<= 1
+	}
+	seg := make([]E, size<<1)
+	for i := range seg {
+		seg[i] = res.e()
+	}
+	for i := 0; i < n; i++ {
+		seg[i+size] = leaves[i]
+	}
+	for i := size - 1; i > 0; i-- {
+		seg[i] = res.op(seg[i<<1], seg[i<<1|1])
+	}
+	res.n = n
+	res.size = size
+	res.seg = seg
+	return res
+}
+func (st *SegmentTree) Get(index int) E {
+	if index < 0 || index >= st.n {
+		return st.e()
+	}
+	return st.seg[index+st.size]
+}
+func (st *SegmentTree) Set(index int, value E) {
+	if index < 0 || index >= st.n {
+		return
+	}
+	index += st.size
+	st.seg[index] = value
+	for index >>= 1; index > 0; index >>= 1 {
+		st.seg[index] = st.op(st.seg[index<<1], st.seg[index<<1|1])
+	}
+}
+func (st *SegmentTree) Update(index int, value E) {
+	if index < 0 || index >= st.n {
+		return
+	}
+	index += st.size
+	st.seg[index] = st.op(st.seg[index], value)
+	for index >>= 1; index > 0; index >>= 1 {
+		st.seg[index] = st.op(st.seg[index<<1], st.seg[index<<1|1])
+	}
+}
+
+// [start, end)
+func (st *SegmentTree) Query(start, end int) E {
+	if start < 0 {
+		start = 0
+	}
+	if end > st.n {
+		end = st.n
+	}
+	if start >= end {
+		return st.e()
+	}
+	leftRes, rightRes := st.e(), st.e()
+	start += st.size
+	end += st.size
+	for start < end {
+		if start&1 == 1 {
+			leftRes = st.op(leftRes, st.seg[start])
+			start++
+		}
+		if end&1 == 1 {
+			end--
+			rightRes = st.op(st.seg[end], rightRes)
+		}
+		start >>= 1
+		end >>= 1
+	}
+	return st.op(leftRes, rightRes)
+}
+func (st *SegmentTree) QueryAll() E { return st.seg[1] }
+func (st *SegmentTree) GetAll() []E {
+	res := make([]E, st.n)
+	copy(res, st.seg[st.size:st.size+st.n])
+	return res
+}
+
+// 二分查询最大的 right 使得切片 [left:right] 内的值满足 predicate
+func (st *SegmentTree) MaxRight(left int, predicate func(E) bool) int {
+	if left == st.n {
+		return st.n
+	}
+	left += st.size
+	res := st.e()
+	for {
+		for left&1 == 0 {
+			left >>= 1
+		}
+		if !predicate(st.op(res, st.seg[left])) {
+			for left < st.size {
+				left <<= 1
+				if tmp := st.op(res, st.seg[left]); predicate(tmp) {
+					res = tmp
+					left++
+				}
+			}
+			return left - st.size
+		}
+		res = st.op(res, st.seg[left])
+		left++
+		if (left & -left) == left {
+			break
+		}
+	}
+	return st.n
+}
+
+// 二分查询最小的 left 使得切片 [left:right] 内的值满足 predicate
+func (st *SegmentTree) MinLeft(right int, predicate func(E) bool) int {
+	if right == 0 {
+		return 0
+	}
+	right += st.size
+	res := st.e()
+	for {
+		right--
+		for right > 1 && right&1 == 1 {
+			right >>= 1
+		}
+		if !predicate(st.op(st.seg[right], res)) {
+			for right < st.size {
+				right = right<<1 | 1
+				if tmp := st.op(st.seg[right], res); predicate(tmp) {
+					res = tmp
+					right--
+				}
+			}
+			return right + 1 - st.size
+		}
+		res = st.op(st.seg[right], res)
+		if right&-right == right {
+			break
+		}
+	}
+	return 0
+}
