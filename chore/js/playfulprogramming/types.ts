a830ddfff19d5827172347d208195665984b9f9e
liaomingtian <liaomingtian@bytedance.com>
2025-08-05 11:55:47 +0800

    feat: 新增可重用组件 Typography 和类型提取工具 ExtractFromDelimiters

---
+++ chore/js/playfulprogramming/types.ts
@@ -0,0 +1,19 @@
+// 非常酷的类型编程
+// https://playfulprogramming.com/posts/fun-with-types
+// https://github.com/ronami/meta-typing
+// https://github.com/ronami/HypeScript
+// https://github.com/codemix/ts-sql
+// https://www.learningtypescript.com/articles/extreme-explorations-of-typescripts-type-system
+
+type ExtractFromDelimiters<S extends string, L extends string, R extends string> = string extends S
+  ? string[] // 如果字符串不是字符串字面量，类型不会出错
+  : S extends ''
+  ? []
+  : S extends `${infer _Head}${L}${infer U}${R}${infer Tail}`
+  ? [U, ...ExtractFromDelimiters<Tail, L, R>]
+  : []
+
+type Test = ExtractFromDelimiters<'Hello, {{name}}!', '{{', '}}'> // Expected: ['name']
+type Test2 = ExtractFromDelimiters<'{{name}} {{age}}', '{{', '}}'> // Expected: ['name', 'age']
+
+export {}
